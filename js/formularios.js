8c6aab52b169c6914cd4f578348204ce52ce6d7a
joao pedro <joaopedrobarreto1000@gmail.com>
2022-06-26 21:30:47 -0300

    correção do erro no jquery ON no envio do formulario

--- js/formularios.js
+++ js/formularios.js
@@ -6,10 +6,12 @@ $(function(){
     });*/
 
     $('body').on('submit','form',function(){
-        alert('enviado!');
-        var url_atual = window.location.href;
-        console.log(url_atual);
-        $('form').html('<h2>Qual o seu melhor e-mail?</h2><input type="email" name"email" required /><input type="submit" value="Enviar" name="acao"/>');
+        alert('formulario enviado');
+        //$('form').html('<input type="email" name"email" required /><input type="submit" value="Enviar" name="acao"/>');
+        $('input[type=email]').val('');
+        $('input[type=text]').val('');
+        $('input[type=number]').val('');
+        $('textarea').val('');
         return false;
     })
 })
